bae47afb41189cb93a286d3c509ffcc5690a1449
Mikaël Ruffieux <mikael@redoxdigital.ch>
2023-04-04 23:19:55 +0200

    fix: jakarta font import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,45 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout';
+import '@/styles/globals.scss';
+import type { AppProps } from 'next/app';
 
-import localFont from 'next/font/local'
+import localFont from 'next/font/local';
 
-const circular = localFont({
+const jakarta = localFont({
   src: [
     {
       path: '../styles/assets/fonts/CircularStd-Black.otf',
       weight: '700',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../styles/assets/fonts/CircularStd-Bold.otf',
       weight: '600',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../styles/assets/fonts/CircularStd-Medium.otf',
       weight: '500',
-      style: 'normal'
+      style: 'normal',
     },
     {
       path: '../styles/assets/fonts/CircularStd-Book.otf',
       weight: '400',
-      style: 'normal'
+      style: 'normal',
     },
-
   ],
-})
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <style jsx global>{`
         :root {
-          --circular-font: ${circular.style.fontFamily};
+          --jakarta-font: ${jakarta.style.fontFamily};
         }
       `}</style>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-     </>
-  )
+    </>
+  );
 }
