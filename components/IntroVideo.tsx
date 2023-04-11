e2212c9fc9d15d4b19a87624b2574edf7c816bfb
Mikaël Ruffieux <mikael@redoxdigital.ch>
2023-04-11 14:15:38 +0200

    fix: small SEO fix

--- components/IntroVideo.tsx
+++ components/IntroVideo.tsx
@@ -1,6 +1,6 @@
 export default function IntroVideo() {
   return (
-    <video autoPlay muted loop>
+    <video autoPlay muted loop aria-hidden>
       <source src="/layouts/PC-Video_BG-V1-1080-LowBitrate.mp4" type="video/mp4" />
     </video>
   );
